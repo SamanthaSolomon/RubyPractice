912558d0086093a4e74152ce4b51a872649bbff1
Samantha Solomon <sleahsolomon@gmail.com>
2021-07-13 16:35:58 -0400

    print puts variables

---
+++ scratch.rb
@@ -0,0 +1,26 @@
+# puts "hank"
+# puts "is a bitch"
+# # print things out on different lines
+# print "Samantha" #no new line
+
+#reads code from the top down
+# puts "/____|"
+# puts "    /|"
+# puts "   / |"
+# puts "  /  |"
+# puts " /   |"
+
+#variables
+# character_name = "Clint"
+# character_age = "35"
+
+# puts ("There once was a man named " + character_name)
+# puts ("he was " + character_age + " years old.")
+
+# # variables can be redefined
+# character_name = "Daisy"
+# puts ("He really liked the name " + character_name)
+# puts ("but didn't like being " + character_age + ".")
+
+#Data Types
+
